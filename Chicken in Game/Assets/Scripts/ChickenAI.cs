c6ab5504b5f40f1c0a55057d60a987ed7d826457
Unknown <emyamodio@gmail.com>
2017-10-23 19:36:57 -0600

    Update Chicken

---
+++ Chicken in Game/Assets/Scripts/ChickenAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ChickenAI : MonoBehaviour {
+	public Rigidbody enemy;
+	public float moveSpeed;
+	public Transform target;
+
+	void OnTriggerStay(Collider other){
+
+		if(other.gameObject.name == "Player"){
+	
+			Debug.Log("Player has entered chickens trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+									   //back//
+		}
+	}
+}
