a7db23cd3ca51ed06edb211ac453fd4ebacdfc34
Unknown <emyamodio@gmail.com>
2017-11-06 20:08:41 -0700

    6/11/2017

---
+++ Chicken in Game/Assets/Scripts/Generic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generic : MonoBehaviour {
+	// // Position
+	// public float posX;
+	// public float posY;
+	// public float posZ;
+	public Vector3 pos;
+	//Rotate
+	// public float rotX;
+	// public float rotY;
+	// public float rotZ;
+	public Quaternion rot;
+	// public float rotW;
+	
+	// //Scale
+	// public float x = 1f;
+	// public float y = 1f;
+	// public float z = 1f;
+	public Vector3 scale;
+	// Update is called once per frame
+	void Start () {
+		scale = transform.localScale;
+	}
+	void Update () {
+		// transform.position = new Vector3(posX,posY,posZ);
+		// transform.rotation = new Quaternion(rotX,rotY,rotZ,rotW);
+		// transform.localScale = new Vector3(x,y,z);
+
+		transform.position = pos;
+		transform.rotation = rot;
+		scale = Vector3.one;
+	}
+}
