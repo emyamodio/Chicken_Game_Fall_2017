6b67dbeec12e9e31bb23fc0b5611165836427b52
Unknown <emyamodio@gmail.com>
2017-11-15 20:13:59 -0700

    11/15/2017

---
+++ Chicken in Game/Assets/Scripts/wolfHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class wolfHealth : MonoBehaviour {
+
+	public int currentHealth;
+
+	public int maxHealth = 3;
+
+	public Transform spawnPoint;
+
+	public int points;
+
+	// Use this for initialization
+	public void TakeDamage (int amount) {
+		currentHealth -=amount;
+		if(currentHealth <= 0){ 
+		//KeyValuePair score at zero
+		currentHealth=0;
+		print("Wolf is Dead!");
+		// Add points to score for killing wolf		
+		ScoreManager.AddPoints(points);
+		//Move wolf ro spawn point for restart
+		transform.position = spawnPoint.position;
+		transform.rotation = spawnPoint.rotation;
+		//Reset Wolf Health
+		currentHealth = maxHealth;
+		}
+	}
+	
+}
