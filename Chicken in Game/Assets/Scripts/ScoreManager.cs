e36b778def54e1f30a0d0a91b860a3f81d82f121
Unknown <emyamodio@gmail.com>
2017-12-06 20:23:07 -0700

    6/12/2017

--- Chicken in Game/Assets/Scripts/ScoreManager.cs
+++ Chicken in Game/Assets/Scripts/ScoreManager.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class ScoreManager : MonoBehaviour {
 
 public static int score;
 
+public int scoreAmount = score;
+
 public Text text;
 
+public int winScore;
+
+public Text winText;
+
+void Awake(){
+    Time.timeScale = 1;
+}
 // Use this for initialization
 void Start () {
+winText.GetComponent<Text>().enabled = false;  
 text = GetComponent<Text>();
 score = 0;
 }
@@ -20,6 +31,16 @@ if(score < 0)
 score = 0;
 
 text.text = " " + score;
+
+if (winScore == score){
+    print("Win Score Reached = " + score);
+    winText.GetComponent<Text>().enabled = true; 
+    Time.timeScale = 0; 
+}
+
+if(Input.GetKeyDown("Esc")){
+    SceneManager.LoadScene(0);
+    }
 }
 
 public static void AddPoints(int pointsToAdd){
@@ -28,6 +49,6 @@ score += pointsToAdd;
 
 public void Reset(){
 score = 0;
-}
+    }
 
 }
---
+++ Chicken in Game/Assets/Scripts/Win.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class win : MonoBehaviour {
+    public int winScore;
+    public GameObject scoreManager;
+    int currentScore; 
+    public Text winText;
+
+
+
+    // Use this for initialization
+    void Start () {
+    winText.GetComponent<Text>().enabled = false;   
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        currentScore = scoreManager.gameObject.GetComponentInChildren<ScoreManager>().scoreAmount;
+        
+        if (currentScore ==winScore){
+			print("Current Score ="+ currentScore);
+            winText.GetComponent<Text>().enabled = true;    
+        }
+    }
+}
